50be28e29a4b66e3849873e3463fc4486c34f555
Sebastian009w <ia3266595@gmail.com>
2023-05-04 15:35:05 -0500

    createContext

---
+++ src/context/Context.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode } from "react";
+
+interface Data {
+  name: string;
+}
+interface ContextProp {
+  children: ReactNode;
+}
+const context = createContext<Data | null>(null);
+
+export function ContextProvider({ children }: ContextProp) {
+  return (
+    <context.Provider
+      value={{
+        name: "sebastian",
+      }}
+    >
+      {children}
+    </context.Provider>
+  );
+}
