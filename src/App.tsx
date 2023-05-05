7ecca9bbcd0a73869921fc1a520bd3f621e6de55
Sebastian009w <ia3266595@gmail.com>
2023-05-04 22:18:33 -0500

    mi perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { User } from "./utils/types";
 
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
-
+import { Profile } from "./components/Profile";
 
 function App() {
+  const [sebastian, setSebastian] = useState<User | null>(null);
+
+  useEffect(() => {
+    const SEBASTIAN = async () => {
+      const res = await fetch(
+        `${import.meta.env.VITE_GITHUB_USERNAME}/sebastian009w`,
+        {
+          method: "GET",
+        }
+      );
+
+      const data: User = await res.json();
+
+      if (!data) return console.log("User Not found");
+
+      setSebastian({ ...data });
+    };
+
+    SEBASTIAN();
+  }, []);
+
   return (
     <>
       <Header>
         <Navbar />
       </Header>
       <Main>
-        <h1>Hello world</h1>
+        {sebastian ? <Profile username={sebastian} /> : "User Not Found"}
       </Main>
     </>
   );
