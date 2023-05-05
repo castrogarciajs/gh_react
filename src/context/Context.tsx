70e0fc2b4aa75fcb0fb109036dc60b926caa48a1
Sebastian009w <ia3266595@gmail.com>
2023-05-04 21:43:31 -0500

    mi perfil

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,13 +1,32 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { ContextProp, Ctx } from "../utils/types";
 
 export const context = createContext<Ctx | null>(null);
 
+interface User {
+  avatarUrl: string;
+  github: string;
+  login: string;
+}
+
 export function ContextProvider({ children }: ContextProp) {
+  const [username, setUsername] = useState<User | null>();
+
+  const SEBASTIAN = async () => {
+    const res = await fetch(
+      `${import.meta.env.VITE_GITHUB_USERNAME}/sebastian009w`
+    );
+
+    const data: User = await res.json();
+
+    if (!data) return console.log("User Not found");
+
+    console.log(data);
+  };
   return (
     <context.Provider
       value={{
-        name: "sebastian",
+        SEBASTIAN,
       }}
     >
       {children}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,9 +4,10 @@ export interface PropPages extends HTMLAttributes<HTMLElement> {}
 
 export interface FormEventSubmit extends FormEventHandler<HTMLFormElement> {}
 
-export interface Ctx {
-  name: string;
-}
-export interface ContextProp {
+export type Ctx = {
+  SEBASTIAN: () => Promise<void>;
+};
+
+export type ContextProp = {
   children: ReactNode;
-}
+};
