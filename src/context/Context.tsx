40c1d16a4abd56989e4d95597727372a96e8cc55
Sebastian009w <ia3266595@gmail.com>
2023-05-05 11:44:56 -0500

    fecth data

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -6,6 +6,14 @@ export const context = createContext<Ctx | null>(null);
 export function ContextProvider({ children }: ContextProp) {
   const [username, setUsername] = useState<User | null>(null);
 
+  const GET = async (profile) => {
+    const res = await fetch(
+      `${import.meta.env.VITE_GITHUB_USERNAME}/${profile}`
+    );
+
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <context.Provider
       value={{
