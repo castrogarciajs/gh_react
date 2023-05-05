d39b601a99af9b5498ea1b705baaba0876ebf40f
Sebastian009w <ia3266595@gmail.com>
2023-05-05 12:27:47 -0500

    mostrando usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useProvider } from "./hooks/useContext";
 import { User } from "./utils/types";
 
 import { Header } from "./components/Header";
@@ -8,7 +9,7 @@ import { Profile } from "./components/Profile";
 
 function App() {
   const [sebastian, setSebastian] = useState<User | null>(null);
-
+  const { username } = useProvider();
   useEffect(() => {
     const SEBASTIAN = async () => {
       const res = await fetch(
@@ -33,7 +34,13 @@ function App() {
       <Header>
         <Navbar />
       </Header>
-      <Main>{sebastian ? <Profile username={sebastian} /> : "Loading"}</Main>
+      <Main>
+        {!username ? (
+          <Profile username={sebastian} />
+        ) : (
+          <Profile username={username} />
+        )}
+      </Main>
     </>
   );
 }
