f9a617c78a42f1e3c36994d2e6a74865c8e1e3f4
Sebastian009w <ia3266595@gmail.com>
2023-05-08 12:10:30 -0500

    first test

--- src/components/_404.test.tsx
+++ src/components/_404.test.tsx
@@ -1,5 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { UserNotFound } from "./_404";
+
 describe("404", () => {
-  test("should sum dos numeros", () => {
-    expect(1 + 2).toBe(3);
+  test("should true", () => {
+    render(<UserNotFound />);
+
+    expect(screen.getAllByAltText("Not Found")).toBeDefined();
   });
 });
