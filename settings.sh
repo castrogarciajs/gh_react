ea0abb59879518a2da536c65f508012c7bf2eef6
Sebastian009w <ia3266595@gmail.com>
2023-05-08 11:24:32 -0500

    configucacion para jest

--- settings.sh
+++ settings.sh
@@ -5,15 +5,16 @@ echo "que deseas realizar ?"
 read create
 read tests
 
-if [[ $create ]] 
-then
-    mkdir examples
-else
-    echo "vuelve pronto"    
-fi    
+# if [[ $create ]] 
+# then
+#     mkdir examples
+# else
+#     echo "vuelve pronto"    
+# fi    
 
 if [[ $tests ]]
 then
     npm run tests
 else
     echo "vuelve pronto"
+fi
---
+++ tests/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Main } from "../src/components/Main";
+
+test("should return ", () => {
+  const JSX = (
+    <Main>
+      <p>hello world</p>
+    </Main>
+  );
+
+  console.log(JSX);
+});
